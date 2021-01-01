b0faa69a5421a2fa5309031a1ed8691a6c3189bc
Andrew Guterman <andrew.guterman1@gmail.com>
2020-12-31 16:12:38 -0800

    default postgres port

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const massive = require('massive');
 
 const connect = massive({
 	host: process.env.POSTGRES_HOST,
-	port: process.env.POSTGRES_PORT,
+	port: process.env.POSTGRES_PORT || 5432,
 	database: process.env.POSTGRES_DB,
 	user: process.env.POSTGRES_USER,
 	password: process.env.POSTGRES_PASSWORD,
